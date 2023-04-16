a71fd7434a3721102030ffb782059849ee3a83ed
Vais Mahendran <vais.mahendran@gmail.com>
2023-04-15 22:07:47 -0400

    add test coverage

--- lib/prospect_utils.rb
+++ lib/prospect_utils.rb
@@ -1,20 +1,11 @@
 module ProspectUtils
-  def self.draft_eligible?(dob, date = draft_date)
-    draft_age = age(dob, date)
+  def self.draft_eligible?(dob, draft_date, is_north_american = true)
+    draft_age = age(dob, draft_date)
 
     return false if draft_age < 18
-    return false if draft_age > 20
+    return false if draft_age > 20 && is_north_american
 
     true
-  end
-
-  def self.draft_date(year = nil)
-    date = Date.new
-
-    return Date.new(year, 9, 15)      if year
-    return Date.new(date.year, 9, 15) if date <= Date.new(date.year, 9, 15)
-    
-    Date.new(date.year + 1, 9, 15)
   end
 
   def self.age(dob, date)
--- spec/prospect_utils_spec.rb
+++ spec/prospect_utils_spec.rb
@@ -2,44 +2,63 @@ require 'date'
 require_relative '../lib/prospect_utils'
 
 RSpec.describe ProspectUtils do
-  # describe '.draft_eligible?' do
-  #   context 'player who will be 18 years old before September 15 of draft year' do
-  #     it 'returns true' do
-  #       dob        = Date.new(Date.today.year - 18)
-  #       draft_date = Date.new(Date.today.year, 3, 15)
-
-  #       expect(described_class.draft_eligible?(dob, draft_date)).to be true
-  #     end
-  #   end
-
-  #   context 'player who will be 18 years old on September 15 of draft year' do
-  #     it 'returns true' do
-  #       dob        = Date.new(Date.today.year - 18, 9, 15)
-  #       draft_date = Date.new(Date.today.year, 9, 15)
+  describe '.draft_eligible?' do
+    context 'player who will be 17 years old on September 15 of draft year' do
+      it 'returns true' do
+        draft_date = Date.new(2023, 9, 15)
+        dob        = Date.new(2005, 9, 16)
 
 
-  #       expect(described_class.draft_eligible?(dob)).to be true
-  #     end
-  #   end
+        expect(described_class.draft_eligible?(dob, draft_date)).to be false
+      end
+    end
+    
+    context 'player who will be 18 years old on September 15 of draft year' do
+      it 'returns true' do
+        draft_date = Date.new(2023, 9, 15)
+        dob        = Date.new(2005, 9, 15)
 
-  #   context 'player is who 20 years old before December 31st of draft year' do
-  #     it 'returns true' do
-  #       dob        = Date.new(Date.today.year - 20)
-  #       draft_date = Date.new(Date.today.year, 12, 31)
 
-  #       expect(described_class.draft_eligible?(dob)).to be true 
-  #     end
-  #   end
+        expect(described_class.draft_eligible?(dob, draft_date)).to be true
+      end
+    end
 
-  #   context 'player who is greater than 20 years old before December 31st of draft year' do
-  #     it 'returns false' do
-  #       dob        = Date.new(Date.today.year - 21)
-  #       draft_date = Date.new(Date.today.year, 12, 31)
+    context 'player who will be 18 years old before September 15 of draft year' do
+      it 'returns true' do
+        draft_date = Date.new(2023, 9, 15)
+        dob        = Date.new(2005, 6, 28)
 
-  #       expect(described_class.draft_eligible?(dob)).to be true 
-  #     end
-  #   end
-  # end
+        expect(described_class.draft_eligible?(dob, draft_date)).to be true
+      end
+    end
+
+    context 'player is who 20 years old before December 31st of draft year' do
+      it 'returns true' do
+        draft_date = Date.new(2023, 12, 31)
+        dob        = Date.new(2003, 12, 31)
+
+        expect(described_class.draft_eligible?(dob, draft_date)).to be true 
+      end
+    end
+
+    context 'player who is greater than 20 years old before December 31st of draft year' do
+      it 'returns false' do
+        draft_date = Date.new(2023, 12, 31)
+        dob        = Date.new(2002, 12, 31)
+
+        expect(described_class.draft_eligible?(dob, draft_date)).to be false 
+      end
+    end
+
+    context 'player who is greater than 20 years old before December 31st of draft year and is not north american' do
+      it 'returns true' do
+        draft_date = Date.new(2023, 12, 31)
+        dob        = Date.new(2002, 12, 31)
+
+        expect(described_class.draft_eligible?(dob, draft_date, false)).to be true 
+      end
+    end
+  end
 
   describe '.age' do
     context 'a person born 11 months ago' do
